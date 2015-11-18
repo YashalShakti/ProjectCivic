662db8b2a0fa71237536d857067bbf59ab97102d
Yashal Shakti <shaktiyashal@gmail.com>
2015-11-18 08:40:37 +0530

    Android Wear

--- mobile/src/main/java/yashalshakti/projectone/GetLocation.java
+++ mobile/src/main/java/yashalshakti/projectone/GetLocation.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.content.IntentSender;
 import android.location.Location;
 import android.os.Bundle;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
